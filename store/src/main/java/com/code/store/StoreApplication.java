c85fcc8868061f9145374d0f81818dbf815135c2
lsy8658 <socszz@gmail.com>
2026-04-10 21:18:34 +0900

    spring boot

---
+++ store/src/main/java/com/code/store/StoreApplication.java
@@ -0,0 +1,14 @@
+package com.code.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
--- store/src/test/java/com/code/store/StoreApplicationTests.java
+++ store/src/test/java/com/code/store/StoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.code.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class StoreApplicationTests {
 
 	@Test
 	void contextLoads() {
