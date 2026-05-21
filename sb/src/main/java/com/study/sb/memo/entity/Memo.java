fa29baad5e0aaf96baa4ab3bd0caba44b334bc49
lsy8658 <socszz@gmail.com>
2026-05-21 22:56:05 +0900

    spring boot

--- sb/src/main/java/com/study/sb/memo/entity/Memo.java
+++ sb/src/main/java/com/study/sb/memo/entity/Memo.java
@@ -1,4 +1,42 @@
 package com.study.sb.memo.entity;
 
+import java.time.ZonedDateTime;
+
 public class Memo {
+    private Long id;
+    private String content;
+    private ZonedDateTime createAt;
+
+    public Memo () {
+
+    }
+
+    public Memo (Long id, String content) {
+        this.id = id;
+        this.content = content;
+        this.createAt = ZonedDateTime.now(); // 현재시간
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+    public ZonedDateTime getCreateAt () {
+        return this.createAt;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreatedAt (ZonedDateTime createdAt) {
+        this.createAt = createdAt;
+    }
 }
--- sb/src/main/java/com/study/sb/memo/repository/MemoRepository.java
+++ sb/src/main/java/com/study/sb/memo/repository/MemoRepository.java
@@ -1,15 +1,71 @@
 package com.study.sb.memo.repository;
 
+import com.study.sb.memo.entity.Memo;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemoRepository {
-    private final List<String> dataList = List.of("1", "2", "3");
+    private final List<Memo> memos = new ArrayList<>();
 
-    public List<String> findAll () {
-        return this.dataList;
+    // 전체 조회
+    public Optional<List<Memo>> findAll () {
+        return Optional.of(this.memos);
     }
+
+    // 단일 조회
+    public Optional<Memo> findOne (long id) {
+        return this.memos.stream().filter((Memo item) -> {
+            return item.getId() == id;
+        }).findAny();
+    }
+
+    // 아이템 추화
+    public Memo create(String content) {
+        long id = memos.size();
+        Memo newMemo = new Memo(id, content);
+        memos.add(newMemo);
+        return newMemo;
+    }
+
+    // 아이템 삭제
+    public boolean delete (long id) {
+       return this.memos.removeIf((Memo item) -> {
+           return item.getId() == id;
+       });
+    }
+
+    // 아이템 수정
+
 }
 // final "한 번 값이 들어가면 다시 바꿀 수 없음"
+
+// 6강 16:40
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
