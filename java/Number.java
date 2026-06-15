d857843a4ec284bccdcf8a5758452820a10c7827
lsy8658 <socszz@gmail.com>
2026-06-15 23:17:04 +0900

    java

---
+++ java/Number.java
@@ -0,0 +1,10 @@
+/*
+    String s = "like Java";
+
+    length : 길이 => 11
+    toUpperCase : 대문자로 => I LIKE JAVA
+    toLowerCase : 소문자로 => i like java
+    contains : 포함 여부 => s.contains("Java") => true
+    indexOf : 위치 정보 => s.indexOf("Java") => 7
+    lastIndexOf : 마지막 위치 정보 => s.lastIndexOf("a") => 10
+*/
