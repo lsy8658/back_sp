9f60c1ce899f1d32b01646d404ad791e4817dcf0
lsy8658 <socszz@gmail.com>
2026-06-19 18:18:08 +0900

    java

--- java/ArrList.java
+++ java/ArrList.java
@@ -1,16 +1,19 @@
-class Box<T> {
-    T data;
-    public void setData (T data) {
-        this.data = data;
-    }
-}
+import java.util.ArrayList;
 
-public class GenericC {
-    public GenericC() {
-        Box<Integer> iBox = new Box<>();
-        iBox.setData(3);
+public class ArrList {
+    public static void main (String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("김");
+        list.add("이");
+        // list.remove("김");
+        // list.remove("김");
+        
+        // System.out.println(list.get(0));
+        // System.out.println(list.contains("이"));
+        // list.clear();
 
-        Box<String> sBox = new Box<>();
-        sBox.setData("안녕"); 
+        // System.out.println(list.size());
+        list.set(0,"최");
+        System.out.println(list.get(0));
     }
 }
