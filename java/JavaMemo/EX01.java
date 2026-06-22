50376c42ee00273a387f7f0dc0af4798037e2097
lsy8658 <socszz@gmail.com>
2026-06-22 20:52:50 +0900

    java

---
+++ java/JavaMemo/EX01.java
@@ -0,0 +1,43 @@
+package JavaMemo;
+
+public class EX01 {
+   public static void main (String[] args) {
+        // 기본 자료형
+        int a = 10;
+        int b = 20;
+        a = b;  // → 값 자체를 복사
+      
+        // 참조 자료형
+        String c = "사과";
+        String d = "바나나";
+        c = d; // → 객체의 참조값(주소 정보)을 복사 따라서 
+        // c와 d는 같은 "바나나" 객체를 참조하게 된다.
+ 
+
+        // 2 ) 형변환
+
+        int score = 93;
+        float score_f = score; // 생략 가능
+        double score_d = score_f; // 생략 가능
+        
+        // 이렇게 생략 가능한걸 묵시적 형변환이라고 한다.
+
+        double score_d2 = 98.8;
+        int score_2 = (int)score_d2; // 생략 불가능
+        
+        // 이렇게 생략이 불가능한걸 명시적 형변환이라고 한다.
+    
+    }
+}
+
+/*
+    int 정수 4 바이트
+    long 정수 8 바이트
+    float 실수 4 바이트 6~7자리
+    double 실수 8 바이트 15자리 정도
+    boolean 참/거짓 1 바이트
+    char 2 문자 바이트
+*/
+
+
+
